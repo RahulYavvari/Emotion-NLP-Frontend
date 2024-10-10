cd72444b18318d38682b8ae229edee8df657790d
RahulYavvari <rahulyavvari04@gmail.com>
2024-10-10 08:59:56 +0530

    Add docs loading screen

---
+++ app/docs/loading.js
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+            <h1 className="flex h-[100vh] justify-center items-center font-bold text-2xl">Loading...</h1>
+        </>
+    );
+}
+
+export default Home;
--- components/Feedback.js
+++ components/Feedback.js
@@ -13,7 +13,7 @@ const Feedback = () => {
     };
 
     const yesFeedbackHandler = () => {
-
+        
     };
 
     useEffect(() => {
